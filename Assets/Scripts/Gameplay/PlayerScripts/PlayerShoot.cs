9fb5a9ac1a6bc0e3bb49a0cba29922c8d32a28a4
DESKTOP-DNAD0U5\sebas <sebastian.fueloep@gmail.com>
2018-05-15 19:32:19 +0200

    Gunshot sound

--- Assets/Scripts/Gameplay/PlayerScripts/PlayerShoot.cs
+++ Assets/Scripts/Gameplay/PlayerScripts/PlayerShoot.cs
@@ -7,13 +7,20 @@ public class PlayerShoot : MonoBehaviour
     public Transform bulletTransform;
     public GameObject bullet;
     public GameObject bulletNoiseRadius;
+    public AudioSource gunShot;
 
+    private void Start()
+    {
+        gunShot = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            gunShot.pitch = Random.Range(0.8f, 1.3f);
+            gunShot.Play();
             Instantiate(bullet, bulletTransform.position, bulletTransform.rotation);
             bulletNoiseRadius.GetComponent<BulletNoise>().notifyEnemies();
         }
