03cdbd6687d48cd89e14a9396ea0524d1815ee3a
SEBI-PC\Sebastian <sebastian.fueloep@gmail.com>
2018-05-17 20:57:01 +0200

    Pathfinding & Sprites rotated

--- Assets/Scripts/Gameplay/EnemyScrips/EnemyPatrolling.cs
+++ Assets/Scripts/Gameplay/EnemyScrips/EnemyPatrolling.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyPatrolling : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class EnemyPatrolling : MonoBehaviour
 
     Animator anim;
 
+    AIDestinationSetter destinationSetter;
+    AILerp aiLerp;
+
     float enemySpeed;
     float turnSpeed = 5;
     Vector3 patrolPointDir;
@@ -31,15 +35,24 @@ public class EnemyPatrolling : MonoBehaviour
     float angle;
     Quaternion q;
 
+    float maxLastSeenTime = 2;
+
     // Use this for initialization
     void Start()
     {
+        destinationSetter = GetComponent<AIDestinationSetter>();
+        aiLerp = GetComponent<AILerp>();
+
         enemySpeed = enemySpeedWalk;
+        
         currentPatrolIndex = 0;
         currentPatrolPoint = patrolPoints[currentPatrolIndex];
 
         anim = GetComponent<Animator>();
 
+        
+        aiLerp.speed = enemySpeedWalk;
+        
         //randomize the idle animation between zombies
         AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
 
@@ -50,11 +63,11 @@ public class EnemyPatrolling : MonoBehaviour
     void Update()
     {
 
-        if ((Time.realtimeSinceStartup - lastTimeSeenPlayer) > 2)
+        if ((Time.realtimeSinceStartup - lastTimeSeenPlayer) > maxLastSeenTime && isSeeingPlayer == true)
         {
             // when losing sight to player, move back towards the last targeted patrol point
-            enemySpeed = enemySpeedWalk;
-            turnSpeed = turnSpeedWalk;
+            aiLerp.speed = enemySpeedWalk;
+            aiLerp.repathRate = 0.5f;
             isSeeingPlayer = false;
             currentPatrolPoint = patrolPoints[currentPatrolIndex];
 
@@ -81,25 +94,30 @@ public class EnemyPatrolling : MonoBehaviour
             currentPatrolPoint = playerTransform;
             enemySpeed = enemySpeedRun;
             turnSpeed = turnSpeedRun;
+            aiLerp.speed = enemySpeedRun;
+            aiLerp.repathRate = 0;
         }
 
         if (currentPatrolPoint != null)
         {
             if (Vector3.Distance(currentPatrolPoint.position, transform.position) > 0.1f)
             {
-                // get the angle between enemy and the next patrol point
-                patrolPointDir = currentPatrolPoint.position - transform.position;
-                angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg;
+                // // get the angle between enemy and the next patrol point
+                // patrolPointDir = currentPatrolPoint.position - transform.position;
+                // angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg;
 
-                // rotate the enemy toward the next patrol point, after rotation is finished, move towards patrol point
-                q = Quaternion.AngleAxis(angle, Vector3.forward);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, q, turnSpeed);
+                // // rotate the enemy toward the next patrol point, after rotation is finished, move towards patrol point
+                // q = Quaternion.AngleAxis(angle, Vector3.forward);
+                // //transform.rotation = Quaternion.RotateTowards(transform.rotation, q, turnSpeed);
+                
+                // if (transform.rotation == q)
+                // {
+                //     //transform.Translate(Vector3.right * Time.deltaTime * enemySpeed);
+                //     anim.SetBool("isMoving", true);
+                // }
 
-                if (transform.rotation == q)
-                {
-                    transform.Translate(Vector3.right * Time.deltaTime * enemySpeed);
-                    anim.SetBool("isMoving", true);
-                }
+                anim.SetBool("isMoving", true);
+                destinationSetter.target = currentPatrolPoint;
             }
             else
             {
@@ -125,9 +143,14 @@ public class EnemyPatrolling : MonoBehaviour
 
         if (hit.collider.gameObject.tag == "Player")
         {
-            lastTimeSeenPlayer = Time.realtimeSinceStartup;
-            isSeeingPlayer = true;
+            SeesPlayer();
         }
+    }
+
+    public void SeesPlayer()
+    {
+        lastTimeSeenPlayer = Time.realtimeSinceStartup;
+        isSeeingPlayer = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
