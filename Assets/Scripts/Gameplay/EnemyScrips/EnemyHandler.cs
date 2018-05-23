54b32233cb5eea033284f436227d5884691c870f
DESKTOP-DNAD0U5\sebas <sebastian.fueloep@gmail.com>
2018-05-23 14:27:25 +0200

    Enemycount, GameManager

--- Assets/Scripts/Gameplay/EnemyScrips/EnemyHandler.cs
+++ Assets/Scripts/Gameplay/EnemyScrips/EnemyHandler.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour
 {
+    public GameObject gameManager;
+
+    public void Start()
+    {
+        gameManager.GetComponent<GameManager>().enemyCount++;
+    }
+
     public void Death()
     {
         //gameObject.SetActive(false);
+        gameManager.GetComponent<GameManager>().enemyCount--;
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Gameplay/PlayerScripts/PlayerHandler.cs
+++ Assets/Scripts/Gameplay/PlayerScripts/PlayerHandler.cs
@@ -7,8 +7,10 @@ public class PlayerHandler : MonoBehaviour
 {
     Animator anim;
 
-
     public int weapon;
+
+    public GameObject gameManager;
+
 
     private void Start()
     {
@@ -17,9 +19,21 @@ public class PlayerHandler : MonoBehaviour
         anim.SetInteger("WeaponSwap", weapon);
     }
 
+    private void Update()
+    {
+        Debug.Log(gameManager.GetComponent<GameManager>().enemyCount);
+        if (gameManager.GetComponent<GameManager>().enemyCount == 0)
+        {
+            Debug.Log("Victory");
+        }
+    }
+
     public void Death()
     {
+        //GameplayHandler.enemyCount = 0;
         Destroy(gameObject);
         SceneManager.LoadScene(0);
     }
+
+
 }
