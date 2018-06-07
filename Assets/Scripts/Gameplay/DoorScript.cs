ca65a72ea73f5f69ea4675579499e7c5ee493dbc
DESKTOP-DNAD0U5\sebas <sebastian.fueloep@gmail.com>
2018-06-07 14:47:59 +0200

    Furniture, Doors kill enemies

---
+++ Assets/Scripts/Gameplay/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float killSpeed = 1;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && rb.velocity.magnitude > killSpeed)
+        {
+            collision.gameObject.GetComponentInParent<EnemyHandler>().Death();
+        }
+    }
+}
